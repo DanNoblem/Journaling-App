526f86f97fcca2ff26655873fbd550ad68596a85
DanNoble <danielwai999@gmail.com>
2023-04-25 14:01:54 -0400

    Initial Commit

---
+++ Journaling/JournalingApp.swift
@@ -0,0 +1,17 @@
+//
+//  JournalingApp.swift
+//  Journaling
+//
+//  Created by Daniel Wai on 4/25/23.
+//
+
+import SwiftUI
+
+@main
+struct JournalingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
